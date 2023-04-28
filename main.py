b4eaf56f6c88dcb1730483e21d94603ddd7ed170
bi3mer <colan@comcast.net>
2023-04-28 11:23:24 -0400

    removed unused function

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from time import sleep
 
 from QLearning import QLearning
 
-def print_frames(frames):
-    for i, frame in enumerate(frames):
-        print(frame['frame'])
-        print(f"Timestep: {i + 1}")
-        print(f"State: {frame['state']}")
-        print(f"Action: {frame['action']}")
-        print(f"Reward: {frame['reward']}")
-        sleep(.1)
 
 env: gym.Env = gym.make("Taxi-v3", render_mode='ansi')
 env.reset()
